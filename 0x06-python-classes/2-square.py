30bf828cac865ff471de19cb0ce0288dfec7bb4d
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-06-27 21:21:33 +0000

    corrections

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""class that defines a square with size validation"""
+""" square class with optional int param"""
 
 
 class Square:
-    """define a private instance attribute"""
+    """square class with int param"""
+
     def __init__(self, size=0):
-        """initializes a square of length size"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError("size must be >=0")
+            raise ValueError("size must be >= 0")
         self.__size = size
