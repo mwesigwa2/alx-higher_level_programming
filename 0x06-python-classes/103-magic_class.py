d7b8e49d901449e2bd48a5ec241f995ade89ba43
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-06-28 21:21:52 +0000

    bytecode documentation

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""python class that does the same as python bytecode"""
 import math
 
 
 class _MagicClass:
+    """class that does the same as python bytecode"""
     def __init__(self)
