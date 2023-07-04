441943d47b0b4af2352ca3e3116ed1aff5171496
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-07-04 09:38:18 +0000

    5. Max integer - Unittest

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""unittest for max_integer([..])
+"""
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """unitests for 6-max_integer"""
+    def test_ints_unorderd(self):
+        """tests for integers only list"""
+        list1 = [1, 2, 7, 3]
+        self.assertEqual(max_integer(list1), 7)
+
+    def test_repeat(self):
+        """checks max with repeated numbers"""
+        list2 = [1, 2, 2]
+        self.assertEqual(max_integer(list2), 2)
+
+    def test_ordered(self):
+        """checks max in ordered list"""
+        list3 = [1, 2, 3]
+        self.assertEqual(max_integer(list3), 3)
+
+    def test_empty(self):
+        """check empty list"""
+        list4 = []
+        self.assertEqual(max_integer(list4), None)
+
+    def test_str(self):
+        """check string"""
+        list5 = ["emma"]
+        self.assertEqual(max_integer(list5), 'emma')
+
+    def test_float(self):
+        """check float"""
+        list6 = [1.0, 4.5, 3.7]
+        self.assertEqual(max_integer(list6), 4.5)
+
+
+if __name__ == '__main__':
+    unittest.main()
