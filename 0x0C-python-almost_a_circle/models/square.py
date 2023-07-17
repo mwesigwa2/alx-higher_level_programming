5d25951d95d03854d0fb89b8184f551a468c9b22
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-07-17 21:32:04 +0000

    Update the class Square by adding the public getter and setter size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -15,3 +15,42 @@ class Square(Rectangle):
                 f"{self.x}/{self.y} - "
                 f"{self.width}"
                 )
+
+    @property
+    def size(self):
+        """fetches size of Square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """assigns value to width and height"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.width = value
+            self.height = value
+
+    def update(self, *args, **kwargs):
+        """assigns arguments to attributes"""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        elif kwargs and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "size" or key == "width" or key == "height":
+                    self.width = value
+                    self.height = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
