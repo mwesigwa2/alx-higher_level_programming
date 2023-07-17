9d2c620d9ce35e3683a875c20dd989ef036e9b6f
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-07-17 20:49:13 +0000

    class rectangle taht inherits from Base

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -19,3 +19,63 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+
+    @property
+    def width(self):
+        """returns the rectangle width"""
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """initializes rectangle width"""
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        elif width <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = width
+
+    @property
+    def height(self):
+        """returns the rectangle height"""
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """initializes rectangle height"""
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        elif height <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = height
+
+    @property
+    def x(self):
+        """returns x cordinate"""
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        """initializes x coordiante"""
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        elif x < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = x
+
+    @property
+    def y(self):
+        """ returns y coordinate """
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        """initializes y cordinate"""
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        elif y < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = y
