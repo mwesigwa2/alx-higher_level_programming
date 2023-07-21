a35e409afef4aa07dcce26b6dfc3f805da6dfce6
Timothy Kibirige <tmwesigwa2@gmail.com>
2023-07-21 09:33:33 +0000

    add classmethods that  serializes and deserializes in CSV

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -88,3 +88,26 @@ class Base:
                             ])
                     elif cls.__name__ == "Square":
                         writer.writerow([obj.id, obj.size, obj.x, obj.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        # loads object from cls file
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, "r") as file:
+                reader = csv.reader(file)
+                instances = []
+                for row in reader:
+                    if cls.__name__ == "Rectangle":
+                        instance = cls.create(
+                            width=int(row[1]), height=int(row[2]),
+                            x=int(row[3]), y=int(row[4])
+                        )
+                    elif cls.__name__ == "Square":
+                        instance = cls.create(
+                            size=int(row[1]), x=int(row[2]), y=int(row[3])
+                        )
+                    instances.append(instance)
+                return instances
+        except FileNotFoundError:
+            return []
